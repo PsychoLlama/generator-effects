fea728ee05087e66c02d1390623c1e7bc23ab278
Jesse Gibson <JesseTheGibson@gmail.com>
2020-12-07 16:40:15 -0800

    Add effect runner utility
    The runner exchanges a thunk for an effect handle and exposes another
    function to invoke the associated thunk. Associations are efficiently
    maintained through a weak map so garbage collection still works as you'd
    expect from the generator's point of view.

---
+++ src/__tests__/runner.test.js
@@ -0,0 +1,15 @@
+import { defer, run } from '../runner';
+
+describe('Runner', () => {
+  it('creates and executes IO handles', () => {
+    const handle = defer(() => 'result');
+
+    expect(run(handle)).toBe('result');
+  });
+
+  it('returns a failed future if the handle is invalid', () => {
+    run('invalid handle').map(null, (error) => {
+      expect(error.message).toMatch(/not an effect/);
+    });
+  });
+});
---
+++ src/runner.js
@@ -0,0 +1,23 @@
+import Future from './future';
+
+const thunks = new WeakMap();
+
+class Effect {}
+
+export function defer(thunk) {
+  const handle = new Effect();
+  thunks.set(handle, thunk);
+
+  return handle;
+}
+
+export function run(handle) {
+  if (!thunks.has(handle)) {
+    return Future((future) => {
+      future.reject(new Error(`Yielded value was not an effect.`));
+    });
+  }
+
+  const thunk = thunks.get(handle);
+  return thunk(); // Contract: effect thunks should always return a future.
+}
