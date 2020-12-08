b2133bf9aa844cbbc51724047c8db814795aa447
Jesse Gibson <JesseTheGibson@gmail.com>
2020-12-07 21:58:13 -0800

    Use context provider in logging effects

--- src/effects/log.js
+++ src/effects/log.js
@@ -1,12 +1,15 @@
 import Future from '../future';
 import { define } from '../dispatcher';
+import createContext from '../context/context';
+
+export const provider = createContext(console);
 
 export const print = define((getContext, ...values) => {
-  console.log(...values);
+  getContext(provider).log(...values);
   return Future.resolve();
 });
 
 export const error = define((getContext, ...values) => {
-  console.error(...values);
+  getContext(provider).error(...values);
   return Future.resolve();
 });
