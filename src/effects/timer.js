a9731293fcb7245c66da4138eef49dd3f57c6bde
Jesse Gibson <JesseTheGibson@gmail.com>
2020-12-07 22:06:48 -0800

    Fix timeouts regression
    The parameters changed for the `sleep(...)` method causing milliseconds
    to get replaced by `getContext()`, which doesn't coerce to a number and
    inadvertently caused all timers to exit instantly. Whoops.

--- src/effects/timer.js
+++ src/effects/timer.js
@@ -1,7 +1,7 @@
 import Future from '../future';
 import { define } from '../dispatcher';
 
-export const sleep = define((milliseconds) => {
+export const sleep = define((getContext, milliseconds) => {
   return Future((future) => {
     setTimeout(future.resolve, milliseconds);
   });
