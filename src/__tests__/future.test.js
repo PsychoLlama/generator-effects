4c3af570c3f46a2e08e5910d31aaaf07d057c28d
Jesse Gibson <JesseTheGibson@gmail.com>
2020-12-07 16:16:28 -0800

    Add future monad
    Why? Because I wanted generators to be able to execute synchronously OR
    asynchronously depending on the implementation of the underlying effect.
    The generator shouldn't know the difference, and I didn't feel like
    making the interpreter care about that either.
    
    `Future` provides a generic interface for effect composition in the
    lower level of the interpreter.

---
+++ src/__tests__/future.test.js
@@ -0,0 +1,59 @@
+import Future from '../future';
+
+describe('Future', () => {
+  it('exposes success values', () => {
+    const handler = jest.fn();
+    Future((f) => f.resolve('result')).map(handler, jest.fn());
+
+    expect(handler).toHaveBeenCalledWith('result');
+  });
+
+  it('exposes rejected values', () => {
+    const handler = jest.fn();
+    Future((f) => f.reject('failure')).map(jest.fn(), handler);
+
+    expect(handler).toHaveBeenCalledWith('failure');
+  });
+
+  it('only emits success or failure, never both', () => {
+    const yey = jest.fn();
+    const ney = jest.fn();
+    Future((f) => {
+      f.resolve('woot');
+      f.reject('oh noes');
+    }).map(yey, ney);
+
+    expect(yey).toHaveBeenCalledWith('woot');
+    expect(ney).not.toHaveBeenCalled();
+  });
+
+  it('allows asynchronous subscribers', async () => {
+    const promise = Promise.resolve(5);
+    const yey = jest.fn();
+    const ney = jest.fn();
+    Future((f) => promise.then(f.resolve, f.reject)).map(yey, ney);
+
+    await promise;
+
+    expect(yey).toHaveBeenCalledWith(5);
+    expect(ney).not.toHaveBeenCalled();
+  });
+
+  it('maps to another future', () => {
+    const handler = jest.fn();
+    Future((f) => f.resolve('10'))
+      .map(Number)
+      .map(handler);
+
+    expect(handler).toHaveBeenCalledWith(10);
+  });
+
+  it('allows listeners to be omitted', () => {
+    const handler = jest.fn();
+    Future((f) => f.reject('x'))
+      .map(Number)
+      .map(null, handler);
+
+    expect(handler).toHaveBeenCalledWith('x');
+  });
+});
---
+++ src/future.js
@@ -0,0 +1,49 @@
+/**
+ * A future is very much like a promise, except it always executes
+ * synchronously, never swallows errors, and doesn't have a concept of
+ * unhandled rejections.
+ */
+export default function Future(executor) {
+  const future = {
+    value: null,
+    state: 'pending',
+    subscribers: [],
+  };
+
+  function fullfill(state, value) {
+    if (future.state !== 'pending') return;
+    future.value = value;
+    future.state = state;
+    future.subscribers.splice(0).map(observe);
+  }
+
+  function observe(sub) {
+    if (future.state === sub.state) sub.listener(future.value);
+    if (future.state === 'pending') future.subscribers.push(sub);
+  }
+
+  executor({
+    resolve: (x) => fullfill('success', x),
+    reject: (x) => fullfill('failure', x),
+  });
+
+  return {
+    map(successListener, failureListener) {
+      return Future((future) => {
+        observe({
+          state: 'success',
+          listener: (x) => future.resolve((successListener || identity)(x)),
+        });
+
+        observe({
+          state: 'failure',
+          listener: (x) => future.reject((failureListener || identity)(x)),
+        });
+      });
+    },
+  };
+}
+
+function identity(x) {
+  return x;
+}
