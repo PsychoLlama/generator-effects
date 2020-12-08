44655aaa16a5a31769ac03cdcd7e49c1ce27275d
Jesse Gibson <JesseTheGibson@gmail.com>
2020-12-07 18:57:56 -0800

    Use task joining from subroutine

--- examples/timer.js
+++ examples/timer.js
@@ -1,4 +1,4 @@
-import execute, { timer, log } from 'effect-runtime';
+import execute, { timer, log, task } from 'effect-runtime';
 
 execute(function* () {
   yield log.print('Running timeout...');
@@ -11,9 +11,12 @@ execute(function* () {
 });
 
 function* subroutine() {
-  yield log.print('Starting timer from subroutine...');
-  yield timer.sleep(2000);
-  yield log.print('Timer done.');
+  yield log.print('Starting timers from subroutine...');
+
+  // Resolves when both timers are done.
+  yield task.join([timer.sleep(1000), timer.sleep(500)]);
+
+  yield log.print('Timers done.');
 
   return 'value from subroutine';
 }
